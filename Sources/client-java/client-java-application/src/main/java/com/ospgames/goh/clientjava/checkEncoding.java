d55bd823b6fe9a69d756bb74672d60fe715c4a6b
Daniel Tamberg <tamberg@openskyprojects.de>
2009-09-12 14:33:39 +0200

    Temporary test class...

---
+++ Sources/client-java/client-java-application/src/main/java/com/ospgames/goh/clientjava/checkEncoding.java
@@ -0,0 +1,12 @@
+package com.ospgames.goh.clientjava;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Daniel
+ * Date: 12.09.2009
+ * Time: 14:28:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class checkEncoding {
+    System.out.println("Umlautetest: ÄÖÜßäöü");
+}
