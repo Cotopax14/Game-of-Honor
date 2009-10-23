5a327fb917c60a9c42a62a9599015e26f0cb244e
unknown <wilko.kempa@googlemail.com>
2009-10-23 21:25:57 +0200

    Fix Client start script for paths containing spaces

---
+++ Sources/server/services/lobby-service/src/main/java/com/ospgames/goh/server/services/lobbyservice/MaxDistTest.java
@@ -0,0 +1,11 @@
+package com.ospgames.goh.server.services.lobbyservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kempa
+ * Date: 19.10.2009
+ * Time: 22:30:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class MaxDistTest {
+}
